31736b7fc98e458200da0ff3745b8615b0f3f446
Aga Madurska <amadurska@gmail.com>
2012-04-09 13:29:49 +0200

    Added a simple menu with the scan button pointing to appropriate functionality

--- src/com/greenapp/GreenAppActivity.java
+++ src/com/greenapp/GreenAppActivity.java
@@ -9,63 +9,33 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
+/**
+ * Main menu activity
+ *
+ * @author aga
+ */
 public class GreenAppActivity extends Activity {
-	private static final String SUCCESS_SCAN = "Congratualtions! You have recycled another item";
-	private static DatabaseHelper dbHelper;  
 	private Button scanButton;
-	private ListView itemList;
-	private List<String> scannedItems;
-    private Button.OnClickListener mScan = new Button.OnClickListener() {
-        public void onClick(View v) {
-            Intent intent = new Intent("com.google.zxing.client.android.SCAN");
-            intent.setPackage("com.google.zxing.client.android");
-            intent.putExtra("SCAN_MODE", "PRODUCT_MODE");
-            startActivityForResult(intent, 0);
-        }
-    };
+	private Intent scanIntent = new Intent();
+    private Button.OnClickListener mScan;
 
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        dbHelper = new DatabaseHelper(this);
-        scannedItems = dbHelper.getAllItemsDescription();
-        if (scannedItems == null) {
-        	scannedItems = new LinkedList<String>();
-        }
-        itemList = (ListView) findViewById(R.id.itemList);
-        itemList.setAdapter(new ScannedArrayAdapter(this, scannedItems));
-        scanButton = (Button)findViewById(R.id.buttonScan);
-        scanButton.setOnClickListener(mScan);
-        itemList.invalidate();
-    }
-
-    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        if (requestCode == 0) {
-            if (resultCode == RESULT_OK) {
-                String productID = intent.getStringExtra("SCAN_RESULT");
-                // Handle successful scan
-                // Show toast
-                Context context = getApplicationContext();
-                String siteContent = HttpRequestHelper.fetchProductInfo(productID);
-                String content = HtmlParser.getDescription(siteContent);
-
-                CharSequence text = SUCCESS_SCAN;
-                int duration = Toast.LENGTH_SHORT;
-                Toast toast = Toast.makeText(context, text, duration);
-                toast.show();
- 
-                scannedItems.add(content);
-                itemList.invalidate();
-                dbHelper.addItem(scannedItems.size(), content, productID);
-            } else if (resultCode == RESULT_CANCELED) {
-                // Do nothing
+        scanButton = (Button) findViewById(R.id.buttonScan);
+        scanIntent.setClass(this, ScanActivity.class);
+        mScan = new Button.OnClickListener() {
+            public void onClick(View v) {
+            	startActivity(scanIntent);
             }
-        }
+        };
+        scanButton.setOnClickListener(mScan);
     }
 }
